746e1373db74e55b8b237a665cf4da81f20c3e4f
rahmonaliyev2007 <rahmonaliyev2007@icloud.com>
2024-11-14 19:01:29 +0500

    Yuk_tashish-xizmati

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.toggle("active");
+}
+function changeLanguage(language) {
+    alert("Til o'zgartirildi: " + language);
+    
+}
